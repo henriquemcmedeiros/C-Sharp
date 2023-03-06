bfe60631a753c5b8f4fcb85ed0c252537154f2b1
Henrique Medeiros <84346920+henriquemcmedeiros@users.noreply.github.com>
2023-03-06 17:09:44 -0300

    Rpg base

---
+++ Monstro/Monstro/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Heroi
+{
+    public int Vida { get; private set; } = 100;
+    public int Dano { get; private set; } = 20;
+
+    public void Ataque(Monstro monstro, Heroi heroi)
+    {
+        monstro.TomarDano(heroi.Dano);
+        Console.WriteLine($"O herói ataca o monstro e causa {heroi.Dano} de dano");
+        Console.WriteLine($"Vida atual do Monstro: {monstro.Vida}");
+        if (monstro.Vida <= 0)
+        {
+            Console.WriteLine("O monstro foi derotado!");
+        }
+    }
+}
---
+++ Monstro/Monstro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var Heroi = new Heroi();
+        var monstro = new Monstro();
+
+        Console.WriteLine($"Vida inicial do Monstro: {monstro.Vida}");
+
+        while (monstro.Vida > 0)
+        {
+            Thread.Sleep(1000);
+            Heroi.Ataque(monstro, Heroi);
+        }
+    }
+}
